e5093ae73646e583cd1268b0708ce6cd07ccfa1a
Steven Galgano <sgalgano@adjacentlink.com>
2020-09-09 12:24:03 -0400

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# emane-antenna-simple-viewer
-Simple EMANE antenna pattern viewer
+emane-antenna-simple-viewer
+==
+
+A simple EMANE antenna and blockage profile 3D rendering tool using
+[Mayavi][1].
+
+```
+$ emane-antenna-simple-viewer -l antenna30dsector.xml
+```
+
+![EMANE Antenna Simple Viewer](images/antenna30dsector.png)
+
+[1]: http://docs.enthought.com/mayavi/mayavi
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='emane-antenna-simple-viewer',
+      version='0.0.1',
+      description='EMANE Antenna Simple Viewer',
+      long_description='EMANE Antenna Simple Viewer',
+      author='Adjacent Link',
+      author_email='emane at adjacent link dot com',
+      url="https://github.com/sgalgano/emane-antenna-simple-viewer",
+      scripts=['scripts/emane-antenna-simple-viewer'],
+      license = 'BSD'
+      )
